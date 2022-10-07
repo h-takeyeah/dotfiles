9820bb0e003940e9cfe16e68bfbe13cdd5f13397
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-10-08 00:09:51 +0900

    :bug: fix missing pulseaudio sink

--- config/waybar/modules/pulseaudio_onclick.sh
+++ config/waybar/modules/pulseaudio_onclick.sh
@@ -2,12 +2,19 @@
 # on click
 
 # toggle default sink
-num_of_sinks=$(pactl list sinks short | wc -l)
-current_default=$(pacmd list-sinks | grep '* index' | awk '{print $3}')
-next_default=$(echo "($current_default + 1) % $num_of_sinks" | bc)
-pactl set-default-sink $next_default
+sinks=($(pactl list sinks short | awk '{print $1}'))
+num_of_sink=${#sinks[@]}
+current_sink=$(pacmd list-sinks | grep '* index' | awk '{print $3}')
+next_sink_index=0
 
-# move active sink-inputs to new sink
-pactl list sink-inputs short | awk '{print $1}' | while read input_id; do
-  pactl move-sink-input $input_id @DEFAULT_SINK@
+for i in "${!sinks[@]}"
+do
+  if [ "${sinks[$i]}" = "$current_sink" ]
+  then
+    next_sink_index=$(echo "($i + 1) % $num_of_sink" | bc)
+    break
+  else
+    :
+  fi
 done
+pactl set-default-sink ${sinks[$next_sink_index]}
