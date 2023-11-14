0c303b75c87ab535dd58cffe01526dc88ab63499
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-11-14 13:45:45 +0900

    lazy nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,29 +1,34 @@
-return require("packer").startup(function()
+return {
   -- Plugin manager for Neovim
-  use "wbthomason/packer.nvim" -- Packer
+  { "wbthomason/packer.nvim" }, -- Packer
 
   -- LSP and completion
-  use "neovim/nvim-lspconfig" -- Quickstart configs for Nvim LSP
-  use "hrsh7th/nvim-cmp" -- A completion engine plugin
-  use "hrsh7th/cmp-nvim-lsp" -- nvim-cmp source for neovim builtin LSP client
+  { "neovim/nvim-lspconfig" }, -- Quickstart configs for Nvim LSP
+  {
+    "hrsh7th/nvim-cmp", -- A completion engine plugin
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client
+    },
+  },
 
   -- LSP manager (successor for nvim-lsp-installer)
-  use "williamboman/mason.nvim"
+  { "williamboman/mason.nvim" },
 
   -- Colorschemes
-  use "morhetz/gruvbox" -- gruvbox
-  use "lifepillar/vim-solarized8" -- true-color solarized
-  use "w0ng/vim-hybrid" -- hybrid
-  use "cocopon/iceberg.vim" -- Bluish color scheme
+  { "morhetz/gruvbox" }, -- gruvbox
+  { "lifepillar/vim-solarized8" }, -- true-color solarized
+  { "w0ng/vim-hybrid" }, -- hybrid
+  { "cocopon/iceberg.vim" }, -- Bluish color scheme
 
   -- Status
-  use "vim-airline/vim-airline" -- Lean & mean status/tabline
+  { "vim-airline/vim-airline" }, -- Lean & mean status/tabline
 
-  use "lewis6991/gitsigns.nvim" -- Git integration for buffers
+  { "lewis6991/gitsigns.nvim" }, -- Git integration for buffers
 
   -- Code format
-  use "editorconfig/editorconfig-vim" -- EditorConfig plugin for Vim
+  { "editorconfig/editorconfig-vim" }, -- EditorConfig plugin for Vim
 
   -- Go
-  use "mattn/vim-goimports" -- Auto-formatting with `:w`
-end)
+  { "mattn/vim-goimports" }, -- Auto-formatting with `:w`
+}
