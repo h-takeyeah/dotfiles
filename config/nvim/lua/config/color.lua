cfaf12c9838f092fdf02545c576accd5e4a351ce
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2025-04-29 13:54:30 +0900

    :lipstick: (nvim)use nice airline theme

--- config/nvim/lua/config/color.lua
+++ config/nvim/lua/config/color.lua
@@ -1,8 +1,4 @@
-vim.opt.background="light"
 local onedark = require("onedark")
 onedark.setup { style = "light" }
 onedark.load()
--- vim.cmd[[
---   colo onedark
---   let g:airline_theme="zenburn"
--- ]]
+vim.g.airline_theme = "sol"
