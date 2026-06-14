90deecf5ff28122ba9b89fdb44b5934ea79eae3a
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2026-06-14 13:50:59 +0900

    nvim add keymap for formatting

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -41,7 +41,7 @@ return {
           vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
           vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
           vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-          --    vim.keymap.set("n", "<space>f", vim.lsp.buf.format({async = true}), bufopts)
+          vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 
           -- See [UI Customization](https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization)
           -- disable virtual_text
